a0088e19acf521b626ae34cb961f553b6cb6180c
RogerJ <827090192@qq.com>
2025-06-11 03:36:30 +0800

    refactor: 调整登录接口返回结构

--- platform-server/src/main/java/com/lmj/platformserver/service/LoginService.java
+++ platform-server/src/main/java/com/lmj/platformserver/service/LoginService.java
@@ -1,9 +1,8 @@
 package com.lmj.platformserver.service;
 
 import com.lmj.platformserver.dto.LoginDTO;
-import com.lmj.platformserver.vo.UserLoginVo;
 import jakarta.servlet.http.HttpSession;
 
 public interface LoginService {
-    UserLoginVo login(LoginDTO loginDTO, HttpSession httpSession);
+    String login(LoginDTO loginDTO, HttpSession httpSession);
 }
--- platform-server/src/main/java/com/lmj/platformserver/vo/UserLoginVo.java
+++
@@ -1,9 +0,0 @@
-package com.lmj.platformserver.vo;
-
-import lombok.Data;
-
-@Data
-public class UserLoginVo {
-    private Long id;
-    private String token;
-}
