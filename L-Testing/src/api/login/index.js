dac6e0861b3781dc89492c40b5160698cd40ecfc
RogerJ <827090192@qq.com>
2025-06-13 01:38:46 +0800

    refactor: 调整用户登录api

--- L-Testing/src/api/login/index.js
+++ L-Testing/src/api/login/index.js
@@ -5,23 +5,21 @@ import request from '@/utils/request';
  * @method login 用户登录
  * @method getCaptcha 获取验证码
  */
-export function useLoginApi() {
-	return {
-		login: (params) => {
-			return request({
-				url: '/login',
-				method: 'post',
-				withCredentials: true,
-				data: params
+export const useLoginApis = {
+    login: (params) => {
+        return request({
+            url: '/login',
+            method: 'post',
+            withCredentials: true,
+            data: params
 
-			});
-		},
-		getCaptcha: () => {
-			return request({
-				url: '/login/captcha',
-				method: 'get',
-				withCredentials: true,
-			})
-		}
-	};
+        });
+    },
+    getCaptcha: () => {
+        return request({
+            url: '/login/captcha',
+            method: 'get',
+            withCredentials: true,
+        })
+    }
 }
