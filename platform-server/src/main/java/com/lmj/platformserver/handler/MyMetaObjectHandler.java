89fa04d68430c1098a1bbce8364d5d60aed03ec8
RogerJ <827090192@qq.com>
2025-06-16 03:46:51 +0800

    feat: 增加mybatis-plus自动填充createUser和updateUser

--- platform-server/src/main/java/com/lmj/platformserver/handler/MyMetaObjectHandler.java
+++ platform-server/src/main/java/com/lmj/platformserver/handler/MyMetaObjectHandler.java
@@ -1,6 +1,7 @@
 package com.lmj.platformserver.handler;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.lmj.platformserver.context.UserContextHolder;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +11,17 @@ import java.time.LocalDateTime;
 public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
+        Long userId = UserContextHolder.getUserId();
+        this.strictInsertFill(metaObject, "createUser", Long.class, userId);
         this.strictInsertFill(metaObject, "createdTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "updateUser", Long.class, userId);
         this.strictInsertFill(metaObject, "updatedTime", LocalDateTime.class, LocalDateTime.now());
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
+        Long userId = UserContextHolder.getUserId();
+        this.strictInsertFill(metaObject, "updateUser", Long.class, userId);
         this.strictUpdateFill(metaObject, "updatedTime", LocalDateTime.class, LocalDateTime.now());
     }
 }
