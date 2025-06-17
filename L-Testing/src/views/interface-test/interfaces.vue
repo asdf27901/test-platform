99fddc48541bdfa762ac468279e4b126a805503b
RogerJ <827090192@qq.com>
2025-06-17 13:13:30 +0800

    style: 修改接口管理布局，将table固定高度

--- L-Testing/src/views/interface-test/interfaces.vue
+++ L-Testing/src/views/interface-test/interfaces.vue
@@ -1,52 +1,54 @@
 <template>
-    <div class="interface-list-page">
-        <!-- 1. 顶部搜索表单 (严格3列布局) -->
-        <el-form :model="searchForm" ref="searchForm" label-width="100px" class="search-form">
-            <el-row :gutter="24">
-                <el-col :span="8">
-                    <el-form-item label="创建用户" prop="userId">
-                        <el-select v-model="searchForm.userId" placeholder="请选择创建用户" clearable style="width: 100%;" filterable>
-                            <el-option
-                                v-for="user in users"
-                                :key="user.id"
-                                :label="user.nickName"
-                                :value="user.id"
-                            ></el-option>
-                        </el-select>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="8">
-                    <el-form-item label="接口名称" prop="name">
-                        <el-input v-model="searchForm.name" placeholder="请输入接口名称" clearable></el-input>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="8">
-                    <el-form-item label="请求方法" prop="method">
-                        <el-select v-model="searchForm.method" placeholder="请选择方法" clearable style="width: 100%;">
-                            <el-option
-                                v-for="item in methodOptions"
-                                :key="item.value"
-                                :label="item.label"
-                                :value="item.value"
-                            >
-                                <el-tag :type="getMethodTagType(item.value)" effect="dark">{{ item.label }}</el-tag>
-                            </el-option>
-                        </el-select>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="8">
-                    <el-form-item label="接口路径" prop="path">
-                        <el-input v-model="searchForm.path" placeholder="请输入接口路径" clearable></el-input>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="8">
-                    <el-form-item label="接口描述" prop="description">
-                        <el-input v-model="searchForm.description" placeholder="请输入接口描述" clearable></el-input>
-                    </el-form-item>
-                </el-col>
-                <el-col :span="8">
-                    <el-form-item label="创建时间" prop="createdTimeRange">
-                        <el-date-picker
+    <!-- [修改] 容器设置为flex布局，与user页面保持一致 -->
+    <div class="page-container">
+        <!-- 1. 顶部搜索表单 -->
+        <div class="filter-container">
+            <el-form :model="searchForm" ref="searchForm" label-width="100px" class="search-form">
+                <el-row :gutter="24">
+                    <el-col :span="8">
+                        <el-form-item label="创建用户" prop="userId">
+                            <el-select v-model="searchForm.userId" placeholder="请选择创建用户" clearable style="width: 100%;" filterable>
+                                <el-option
+                                    v-for="user in users"
+                                    :key="user.id"
+                                    :label="user.nickName"
+                                    :value="user.id"
+                                ></el-option>
+                            </el-select>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-form-item label="接口名称" prop="name">
+                            <el-input v-model="searchForm.name" placeholder="请输入接口名称" clearable></el-input>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-form-item label="请求方法" prop="method">
+                            <el-select v-model="searchForm.method" placeholder="请选择方法" clearable style="width: 100%;">
+                                <el-option
+                                    v-for="item in methodOptions"
+                                    :key="item.value"
+                                    :label="item.label"
+                                    :value="item.value"
+                                >
+                                    <el-tag :type="getMethodTagType(item.value)" effect="dark">{{ item.label }}</el-tag>
+                                </el-option>
+                            </el-select>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-form-item label="接口路径" prop="path">
+                            <el-input v-model="searchForm.path" placeholder="请输入接口路径" clearable @keyup.enter.native="handleSearch"></el-input>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-form-item label="接口描述" prop="description">
+                            <el-input v-model="searchForm.description" placeholder="请输入接口描述" clearable @keyup.enter.native="handleSearch"></el-input>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-form-item label="创建时间" prop="createdTimeRange">
+                            <el-date-picker
                                 v-model="searchForm.createdTimeRange"
                                 type="datetimerange"
                                 range-separator="至"
@@ -54,13 +56,14 @@
                                 end-placeholder="结束日期"
                                 value-format="yyyy-MM-dd HH:mm:ss"
                                 style="width: 100%;">
-                        </el-date-picker>
-                    </el-form-item>
-                </el-col>
-            </el-row>
-        </el-form>
+                            </el-date-picker>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+            </el-form>
+        </div>
 
-        <!-- 2. 顶部操作区 -->
+        <!-- 2. 操作区 -->
         <div class="actions-bar">
             <!-- 左侧搜索操作 -->
             <div class="search-actions">
@@ -71,10 +74,10 @@
             <div class="main-actions">
                 <el-button type="primary" icon="el-icon-plus" @click="handleCreate">创建接口</el-button>
                 <el-button
-                        type="danger"
-                        icon="el-icon-delete"
-                        @click="handleBatchDelete"
-                        :disabled="multipleSelection.length === 0"
+                    type="danger"
+                    icon="el-icon-delete"
+                    @click="handleBatchDelete"
+                    :disabled="multipleSelection.length === 0"
                 >
                     批量删除
                 </el-button>
@@ -82,7 +85,9 @@
         </div>
 
         <!-- 3. 数据表格 -->
-        <el-table
+        <!-- [修改] 添加table-wrapper并绑定ref -->
+        <div class="table-wrapper" ref="tableWrapper">
+            <el-table
                 v-loading="loading"
                 :data="tableData"
                 style="width: 100%"
@@ -91,47 +96,53 @@
                 :header-cell-style="{ background: '#fafafa', color: '#606266', textAlign: 'center' }"
                 cell-class-name="table-cell-center"
                 :row-key="getRowKey"
-        >
-            <el-table-column type="selection" width="55" align="center" reserve-selection></el-table-column>
-            <el-table-column prop="id" label="接口ID" width="80" align="center"></el-table-column>
-            <el-table-column prop="name" label="接口名称" width="200" show-overflow-tooltip align="center"></el-table-column>
-            <el-table-column prop="method" label="请求方法" width="120" align="center">
-                <template #default="{row}">
-                    <el-tag :type="getMethodTagType(row.method)" effect="dark">{{ row.method }}</el-tag>
-                </template>
-            </el-table-column>
-            <el-table-column prop="path" label="接口路径" width="250" show-overflow-tooltip align="center"></el-table-column>
-            <el-table-column prop="description" label="接口描述" show-overflow-tooltip align="center" width="400"></el-table-column>
-            <el-table-column prop="createUser" label="创建用户" width="100" align="center"></el-table-column>
-            <el-table-column prop="updateUser" label="更新用户" width="120" align="center"></el-table-column>
-            <el-table-column prop="createdTime" label="创建时间" width="180" align="center"></el-table-column>
-            <el-table-column prop="updatedTime" label="更新时间" width="180" align="center"></el-table-column>
+                :height="tableHeight"
+                reserve-selection
+            >
+                <el-table-column type="selection" width="55" align="center"></el-table-column>
+                <el-table-column prop="id" label="接口ID" width="80" align="center"></el-table-column>
+                <el-table-column prop="name" label="接口名称" width="200" show-overflow-tooltip align="center"></el-table-column>
+                <el-table-column prop="method" label="请求方法" width="120" align="center">
+                    <template #default="{row}">
+                        <el-tag :type="getMethodTagType(row.method)" effect="dark">{{ row.method }}</el-tag>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="path" label="接口路径" width="250" show-overflow-tooltip align="center"></el-table-column>
+                <el-table-column prop="description" label="接口描述" show-overflow-tooltip align="center" min-width="200"></el-table-column>
+                <el-table-column prop="createUser" label="创建用户" width="100" align="center"></el-table-column>
+                <el-table-column prop="updateUser" label="更新用户" width="120" align="center"></el-table-column>
+                <el-table-column prop="createdTime" label="创建时间" width="180" align="center"></el-table-column>
+                <el-table-column prop="updatedTime" label="更新时间" width="180" align="center"></el-table-column>
 
-            <el-table-column label="操作" width="290" fixed="right" align="center">
-                <template #default="{row}">
-                    <el-button size="mini" type="primary" icon="el-icon-edit" @click="handleEdit(row)">编辑
-                    </el-button>
-                    <el-button size="mini" type="danger" icon="el-icon-delete" @click="handleDelete(row)">删除
-                    </el-button>
-                    <el-button size="mini" type="success" icon="el-icon-document-add"
-                               @click="handleGenerateCase(row)">生成用例
-                    </el-button>
-                </template>
-            </el-table-column>
-        </el-table>
+                <el-table-column label="操作" width="290" fixed="right" align="center">
+                    <template #default="{row}">
+                        <el-button size="mini" type="primary" icon="el-icon-edit" @click="handleEdit(row)">编辑
+                        </el-button>
+                        <el-button size="mini" type="danger" icon="el-icon-delete" @click="handleDelete(row)">删除
+                        </el-button>
+                        <el-button size="mini" type="success" icon="el-icon-document-add"
+                                   @click="handleGenerateCase(row)">生成用例
+                        </el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
 
         <!-- 4. 分页 -->
-        <el-pagination
-                class="pagination"
+        <!-- [修改] 添加pagination-container并绑定ref -->
+        <div class="pagination-container" ref="paginationContainer">
+            <el-pagination
                 @size-change="handleSizeChange"
                 @current-change="handleCurrentChange"
-                :current-page="pagination.currentPage"
+                :current-page="pagination.current"
                 :page-sizes="[10, 20, 50, 100]"
                 :page-size="pagination.size"
                 layout="total, sizes, prev, pager, next, jumper"
                 :total="pagination.total">
-        </el-pagination>
+            </el-pagination>
+        </div>
 
+        <!-- 弹窗部分保持不变 -->
         <create-interface-dialog
             :visible.sync="createDialogVisible"
             @success="handleCreateSuccess"
@@ -147,6 +158,8 @@
 </template>
 
 <script>
+// [修改] 引入 debounce
+import { debounce } from 'lodash';
 import createInterfaceDialog from "@/views/interface-test/components/createInterfaceDialog.vue";
 import {interfaceApis} from "@/api/interface";
 import {Message} from "element-ui";
@@ -156,10 +169,14 @@ import editInterfaceDialog from "@/views/interface-test/components/editInterface
 export default {
     name: 'InterfaceList',
     components: {
-      createInterfaceDialog, editInterfaceDialog
+        createInterfaceDialog, editInterfaceDialog
     },
     data() {
         return {
+            // [修改] 新增 tableHeight 和 debouncedResizeHandler
+            tableHeight: 400, // 默认高度
+            debouncedResizeHandler: null,
+
             // 搜索表单数据
             searchForm: {
                 userId: '',
@@ -175,7 +192,6 @@ export default {
             tableData: [],
             // 多选中的数据
             multipleSelection: [],
-            allSelectedIds: new Set(),
             // 分页信息
             pagination: {
                 current: 1,
@@ -192,15 +208,39 @@ export default {
                 {value: 'PATCH', label: 'PATCH'},
             ],
             createDialogVisible: false,
-            editDialogVisible: false,      // 控制编辑弹窗的显示
-            currentEditingInterface: {},   // 存储当前正在编辑的行数据
+            editDialogVisible: false,
+            currentEditingInterface: {},
         };
     },
     async created() {
-        await this.fetchActiveUser()
+        // [修改] 初始化 debounced handler
+        this.debouncedResizeHandler = debounce(this.resizeTableHeight, 100);
+
+        await this.fetchActiveUser();
         await this.fetchData();
     },
+    // [修改] 新增 mounted 和 beforeDestroy 生命周期钩子
+    mounted() {
+        this.$nextTick(() => {
+            this.resizeTableHeight();
+        });
+        window.addEventListener('resize', this.debouncedResizeHandler);
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.debouncedResizeHandler);
+    },
     methods: {
+        // [修改] 新增 resizeTableHeight 方法
+        resizeTableHeight() {
+            if (this.$refs.tableWrapper && this.$refs.paginationContainer) {
+                const tableTop = this.$refs.tableWrapper.getBoundingClientRect().top;
+                const paginationHeight = this.$refs.paginationContainer.offsetHeight;
+                // 20是page-container的padding-bottom, 20是预留的额外底部间距
+                const availableHeight = window.innerHeight - tableTop - paginationHeight - 20 - 20;
+                this.tableHeight = Math.max(availableHeight, 200); // 最小高度200
+            }
+        },
+
         // 获取数据
         async fetchData() {
             this.loading = true;
@@ -230,6 +270,9 @@ export default {
                 }
             } finally {
                 this.loading = false;
+                this.$nextTick(() => {
+                    this.resizeTableHeight();
+                });
             }
         },
 
@@ -253,8 +296,16 @@ export default {
 
         // 重置搜索表单
         handleReset() {
-            this.$refs.searchForm.resetFields();
-            this.searchForm.createdTimeRange = []; // resetFields 对 el-date-picker 可能无效，手动清空
+            // this.$refs.searchForm.resetFields(); // el-row/el-col 嵌套下 resetFields 可能失效
+            // 手动重置更可靠
+            this.searchForm = {
+                userId: '',
+                name: '',
+                method: '',
+                path: '',
+                description: '',
+                createdTimeRange: [],
+            };
             this.handleSearch();
         },
 
@@ -382,35 +433,45 @@ export default {
 </script>
 
 <style scoped>
-.interface-list-page {
+.page-container {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
     padding: 20px;
-    background-color: #fff; /* 改为白色 */
-    min-height: calc(100vh - 50px);
+    background-color: #fff;
+    border-radius: 4px;
+    box-sizing: border-box;
 }
 
-/*.search-form .el-row {*/
-/*    margin-bottom: 20px;*/
-/*}*/
+.filter-container {
+    flex-shrink: 0;
+}
 
 .search-form .el-col {
     margin-bottom: 20px;
 }
 
-.search-form .el-form-item {
-    margin-bottom: 20px;
-}
-
-/* 新增：操作栏容器，使用Flexbox实现两端对齐 */
 .actions-bar {
+    flex-shrink: 0;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    margin-bottom: 20px; /* 与下方表格的间距 */
+    margin-bottom: 20px;
 }
 
-.pagination {
+.table-wrapper {
+    flex: 1;
+    overflow: hidden;
+}
+
+.pagination-container {
+    flex-shrink: 0;
     margin-top: 20px;
-    display: flex;
-    justify-content: flex-end;
+    text-align: right;
+}
+
+/* 确保表格内的单元格内容居中 */
+::v-deep .table-cell-center {
+    text-align: center;
 }
 </style>
