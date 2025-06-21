49c1686e7145115b2e1c072aa18700b68aa551f2
RogerJ <827090192@qq.com>
2025-06-22 04:38:04 +0800

    feat: 新增批量添加接口时对空数组的校验

--- platform-server/src/main/java/com/lmj/platformserver/dto/SaveInterfacesDTO.java
+++ platform-server/src/main/java/com/lmj/platformserver/dto/SaveInterfacesDTO.java
@@ -2,6 +2,8 @@ package com.lmj.platformserver.dto;
 
 import com.lmj.platformserver.entity.Interface;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.List;
@@ -10,5 +12,7 @@ import java.util.List;
 public class SaveInterfacesDTO {
 
     @Valid
+    @NotEmpty(message = "接口为空")
+    @NotNull(message = "接口为空")
     private List<Interface> interfaces;
 }
