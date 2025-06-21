8e7d9bb803de7da71bd94e785c652737cf7767b1
RogerJ <827090192@qq.com>
2025-06-22 02:19:54 +0800

    refactor: 调整sql脚本

--- server.sql
+++ server.sql
@@ -57,12 +57,13 @@ CREATE TABLE Interfaces
 
 CREATE INDEX idx_interfaces_user_id_is_deleted ON Interfaces (create_user, is_deleted);
 
-CREATE TABLE TestCases
+CREATE TABLE Interface_testcases
 (
-    id                           INT AUTO_INCREMENT PRIMARY KEY COMMENT '测试用例ID',
+    id                           INT AUTO_INCREMENT PRIMARY KEY COMMENT '接口测试用例ID',
     interface_id                      INT                  NOT NULL COMMENT '关联的接口ID',
-    name                              VARCHAR(100)         NOT NULL COMMENT '测试用例名称',
+    name                              VARCHAR(100)         NOT NULL COMMENT '接口测试用例名称',
     priority                          INT        DEFAULT 0 COMMENT '用例优先级，0-高, 1-中, 2-低，数值越小优先级越高',
+    host                              varchar(100)         not null comment '域名地址',
     request_method                    VARCHAR(10)          NOT NULL COMMENT '请求方法',
     request_body_type                 VARCHAR(30) COMMENT '请求体类型',
     request_path_variables            JSON COMMENT '请求路径参数',
@@ -82,5 +83,5 @@ CREATE TABLE TestCases
     updated_time                      TIMESTAMP            NOT NULL COMMENT '更新时间'
 );
 
-CREATE INDEX idx_testcases_interface_id_is_deleted ON TestCases (interface_id, is_deleted); -- 方便查询某个接口下未删除的用例
-CREATE INDEX idx_testcases_user_id_is_deleted ON TestCases (create_user, is_deleted); -- 方便查询某个用户创建的未删除用例
+CREATE INDEX idx_testcases_interface_id_is_deleted ON Interface_testcases (interface_id, is_deleted); -- 方便查询某个接口下未删除的用例
+CREATE INDEX idx_testcases_user_id_is_deleted ON Interface_testcases (create_user, is_deleted); -- 方便查询某个用户创建的未删除用例
