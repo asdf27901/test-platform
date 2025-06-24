18fb174bdbf5a2a98a6c46b8098f44da659df169
RogerJ <827090192@qq.com>
2025-06-25 04:13:36 +0800

    feat: 新增测试用例文件上传接口

--- platform-server/src/main/java/com/lmj/platformserver/controller/FileController.java
+++ platform-server/src/main/java/com/lmj/platformserver/controller/FileController.java
@@ -3,6 +3,7 @@ package com.lmj.platformserver.controller;
 import com.lmj.platformserver.annotation.validation.File;
 import com.lmj.platformserver.result.Response;
 import com.lmj.platformserver.utils.AliOssUtil;
+import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -23,8 +24,20 @@ public class FileController {
     @PostMapping("/uploadImage")
     public Response<String> uploadImage(
             @File(maxSize = 2, allowFileTypes = {"jpg", "jpeg", "png"})
+            @NotNull(message = "文件不能为空")
             MultipartFile file) {
         log.info("图片上传: {}", file.getOriginalFilename());
+        String path = aliOssUtil.upload(file);
+        return Response.success(path);
+    }
+
+    @PostMapping("/uploadTestcaseFile")
+    public Response<String> uploadTestcaseFile(
+            @File(maxSize = 10, allowFileTypes = {})
+            @NotNull(message = "文件不能为空")
+            MultipartFile file
+    ) {
+        log.info("测试用例文件上传：{}", file.getOriginalFilename());
         String path = aliOssUtil.upload(file);
         return Response.success(path);
     }
