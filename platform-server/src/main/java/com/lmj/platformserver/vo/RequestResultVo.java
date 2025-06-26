430ce8a9d59981837a6d748b12541d664219f207
RogerJ <827090192@qq.com>
2025-06-27 01:48:59 +0800

    feat: 完成测试用例请求发送接口开发

---
+++ platform-server/src/main/java/com/lmj/platformserver/vo/RequestResultVo.java
@@ -0,0 +1,15 @@
+package com.lmj.platformserver.vo;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Map;
+
+@Data
+public class RequestResultVo implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Map<String, Object> response;
+}
