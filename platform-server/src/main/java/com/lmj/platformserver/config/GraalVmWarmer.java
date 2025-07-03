ead1a331b48778eb2928ce1b0de8371380e78171
RogerJ <827090192@qq.com>
2025-07-03 23:44:38 +0800

    feat: 新增GraalVM配置

---
+++ platform-server/src/main/java/com/lmj/platformserver/config/GraalVmWarmer.java
@@ -0,0 +1,20 @@
+package com.lmj.platformserver.config;
+
+import com.lmj.platformserver.assertion.PostAssertionTool;
+import com.lmj.platformserver.service.JsScriptExecutionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GraalVmWarmer implements ApplicationRunner {
+
+    @Autowired
+    private JsScriptExecutionService scriptService;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        scriptService.executeJsScript("console.log('Engine warmed up!')", new PostAssertionTool());
+    }
+}
