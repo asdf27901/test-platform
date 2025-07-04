1a383a1045a27c5f21970ee3f4f663563afbd1c9
RogerJ <827090192@qq.com>
2025-07-05 03:41:33 +0800

    refactor: 调整sql脚本

--- server.sql
+++ server.sql
@@ -22,23 +22,20 @@ CREATE INDEX idx_users_is_deleted_is_active ON Users (is_deleted, is_active); --
 insert into Users (username, password, avatar_url, nickname, created_time, updated_time)
 values ('admin', '123456', null, '管理员', '2026-06-06 23:42:42', '2026-06-06 23:42:42');
 
-CREATE TABLE Environments
+CREATE TABLE environment_variables
 (
     id       INT AUTO_INCREMENT PRIMARY KEY COMMENT '环境ID',
-    user_id      INT                  NOT NULL COMMENT '所属用户ID',
     name         VARCHAR(100)         NOT NULL COMMENT '环境名称',
-    base_url     VARCHAR(255)         NOT NULL COMMENT '环境基础URL',
-    description  TEXT COMMENT '环境描述',
     variables    JSON COMMENT '环境级别的通用变量',
     is_deleted   TINYINT(1) DEFAULT 0 NOT NULL COMMENT '逻辑删除标志，0表示未删除，1表示已删除',
     version      INT        DEFAULT 0 NOT NULL COMMENT '乐观锁版本号',
+    create_user  int                  not null comment '创建用户ID',
     created_time TIMESTAMP            NOT NULL COMMENT '创建时间',
-    updated_time TIMESTAMP            NOT NULL COMMENT '更新时间',
-
-    UNIQUE (user_id, name, is_deleted) COMMENT '唯一约束：同一个用户下的环境名称必须唯一(逻辑删除除外)'
+    update_user  int                  not null comment '更新用户ID',
+    updated_time TIMESTAMP            NOT NULL COMMENT '更新时间'
 );
 
-CREATE INDEX idx_environments_user_id_is_deleted ON Environments (user_id, is_deleted); -- 方便查询某个用户下未删除的环境
+CREATE INDEX idx_environment_variables_create_user_is_deleted ON environment_variables (create_user, is_deleted); -- 方便查询某个用户创建的未删除环境
 
 CREATE TABLE Interfaces
 (
@@ -55,14 +52,14 @@ CREATE TABLE Interfaces
     updated_time TIMESTAMP            NOT NULL COMMENT '更新时间'
 );
 
-CREATE INDEX idx_interfaces_user_id_is_deleted ON Interfaces (create_user, is_deleted);
+CREATE INDEX idx_interfaces_create_user_is_deleted ON Interfaces (create_user, is_deleted); -- 方便查询某个用户创建的未删除接口
 
 CREATE TABLE Interface_testcases
 (
     id                           INT AUTO_INCREMENT PRIMARY KEY COMMENT '接口测试用例ID',
     interface_id                      INT                  NOT NULL COMMENT '关联的接口ID',
     name                              VARCHAR(100)         NOT NULL COMMENT '接口测试用例名称',
-    priority                          INT        DEFAULT 0 COMMENT '用例优先级，0-高, 1-中, 2-低，数值越小优先级越高',
+    priority                          INT        DEFAULT 0 COMMENT '用例优先级，0-高, 1-中, 2-低',
     host                              varchar(100)         not null comment '域名地址',
     request_method                    VARCHAR(10)          NOT NULL COMMENT '请求方法',
     request_body_type                 VARCHAR(30) COMMENT '请求体类型',
@@ -70,9 +67,6 @@ CREATE TABLE Interface_testcases
     request_query_params              JSON COMMENT '请求查询参数',
     request_headers                   JSON COMMENT '特定于此用例的请求头',
     request_body                      JSON COMMENT '特定于此用例的请求体数据',
-    expected_status_code              INT COMMENT '期望的HTTP响应状态码',
-    expected_response_headers         JSON COMMENT '期望的响应头断言规则',
-    expected_response_body_assertions JSON COMMENT '期望的响应体断言规则',
     pre_request_script                TEXT COMMENT '请求前置脚本',
     post_request_script               TEXT COMMENT '请求后置脚本/断言脚本',
     is_deleted                        TINYINT(1) DEFAULT 0 NOT NULL COMMENT '逻辑删除标志，0表示未删除，1表示已删除',
@@ -84,4 +78,4 @@ CREATE TABLE Interface_testcases
 );
 
 CREATE INDEX idx_testcases_interface_id_is_deleted ON Interface_testcases (interface_id, is_deleted); -- 方便查询某个接口下未删除的用例
-CREATE INDEX idx_testcases_user_id_is_deleted ON Interface_testcases (create_user, is_deleted); -- 方便查询某个用户创建的未删除用例
+CREATE INDEX idx_testcases_create_user_is_deleted ON Interface_testcases (create_user, is_deleted); -- 方便查询某个用户创建的未删除用例
