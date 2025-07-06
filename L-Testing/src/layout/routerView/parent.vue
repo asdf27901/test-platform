1f2e967c685914cfbdd1a2618394f23c89f164fc
RogerJ <827090192@qq.com>
2025-07-07 01:45:28 +0800

    refactor: 撤回放开路由不一致无法刷新的限制

--- L-Testing/src/layout/routerView/parent.vue
+++ L-Testing/src/layout/routerView/parent.vue
@@ -21,11 +21,14 @@ export default {
 	created() {
 		// 页面加载前，处理缓存，页面刷新时路由缓存处理
 		this.keepAliveNameList = this.getKeepAliveNames();
-		this.bus.$on('onTagsViewRefreshRouterView', () => {
-			// if (this.$route.path !== path) return false;
+		this.bus.$on('onTagsViewRefreshRouterView', (path) => {
+			if (this.$route.path !== path) return false;
 			this.keepAliveNameList = this.getKeepAliveNames().filter((name) => this.$route.name !== name);
 			this.refreshRouterViewKey = this.$route.path;
 			this.$nextTick(() => {
+				// 这里设置为将refreshRouterViewKey为null是为了更新绑定在组件的key
+				// 如果keep-alive没有包含当前组件，那么当key发生变更时，就会销毁重新创建组件
+				// 如果keep-alive包含当前组件，那么key发生变更时，也只是触发组件的activated方法
 				this.refreshRouterViewKey = null;
 				this.keepAliveNameList = this.getKeepAliveNames();
 			});
