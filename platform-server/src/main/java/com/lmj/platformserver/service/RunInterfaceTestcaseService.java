3ef60e5d2847739a52d42a1a42db5839a19c3cd6
RogerJ <827090192@qq.com>
2025-08-02 00:27:27 +0800

    refactor: 重构执行用例方法

--- platform-server/src/main/java/com/lmj/platformserver/service/RunInterfaceTestcaseService.java
+++ platform-server/src/main/java/com/lmj/platformserver/service/RunInterfaceTestcaseService.java
@@ -1,14 +1,14 @@
 package com.lmj.platformserver.service;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import com.lmj.platformserver.assertion.PostAssertionTool;
 import com.lmj.platformserver.assertion.PreAssertionTool;
+import com.lmj.platformserver.constant.RequestResultConstant;
 import com.lmj.platformserver.context.ApiRequestLogsContextHolder;
-import com.lmj.platformserver.entity.ApiRequestLogs;
-import com.lmj.platformserver.entity.EnvironmentVariable;
-import com.lmj.platformserver.entity.Interface;
-import com.lmj.platformserver.mapper.EnvironmentVariableMapper;
+import com.lmj.platformserver.context.UserContextHolder;
+import com.lmj.platformserver.entity.*;
 import com.lmj.platformserver.pojo.RequestSteps;
 import com.lmj.platformserver.utils.HttpUtil;
 import com.lmj.platformserver.utils.VariableResolver;
@@ -19,7 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class RunInterfaceTestcaseService {
@@ -27,12 +27,12 @@ public class RunInterfaceTestcaseService {
     @Autowired
     private JsScriptExecutionService jsScriptExecutionService;
     @Autowired
-    private EnvironmentVariableMapper environmentVariableMapper;
+    private EnvironmentVariableService environmentVariableService;
     @Autowired
     private HttpUtil httpUtil;
 
     public RequestResultVo runSingleRequest(
-            Interface api,
+            String path,
             EnvironmentVariable environmentVariable,
             Map<String, Object> requestData
     ) {
@@ -40,54 +40,104 @@ public class RunInterfaceTestcaseService {
         if (apiRequestLogs == null) {
             apiRequestLogs = new ApiRequestLogs();
         }
-        return runSingleRequest(api, environmentVariable, requestData, apiRequestLogs);
+        return (RequestResultVo) runRequest(path, environmentVariable, requestData, apiRequestLogs, false);
     }
 
     @SuppressWarnings("unchecked")
-    private RequestResultVo runSingleRequest(
-            Interface api,
+    private Object runRequest(
+            String path,
             EnvironmentVariable environmentVariable,
             Map<String, Object> requestData,
-            ApiRequestLogs apiRequestLogs
+            ApiRequestLogs apiRequestLogs,
+            boolean isChain
     ) {
-        RequestResultVo resultVo = new RequestResultVo();
-        RequestSteps steps = new RequestSteps();
-        apiRequestLogs.getSteps().add(steps);
-        apiRequestLogs.setInterfaceId(api.getId());
-        steps.setInterfaceId(api.getId());
+        RequestSteps step = new RequestSteps();
+        apiRequestLogs.getSteps().add(step);
+        step.setInterfaceId((Long) requestData.get("interfaceId"));
+        step.setTestcaseId((Long) requestData.get("id"));
 
-        Map<String, Object> requestDataMapper = httpUtil.processRequestDataForHttp(requestData);
-        String preRequestScript = (String) requestData.get("preRequestScript");
-        String postRequestScript = (String) requestData.get("postRequestScript");
-        ScriptExecutionResultVo preScriptResult = executeScript(requestDataMapper, null, environmentVariable, preRequestScript, true);
-        resultVo.setPreExecutionResult(preScriptResult);
-        steps.setPreScriptData(preScriptResult);
+        Map<String, Object> requestDataMap = httpUtil.processRequestDataForHttp(requestData);
+        ScriptExecutionResultVo preScriptResult = executeScript(requestDataMap, null, environmentVariable, (String) requestData.get("preRequestScript"), true);
+
+        step.setPreScriptData(preScriptResult);
         if (environmentVariable != null) {
-            requestDataMapper = (Map<String, Object>) VariableResolver.resolve(environmentVariable.getVariables(), requestDataMapper);
+            requestDataMap = (Map<String, Object>) VariableResolver.resolve(environmentVariable.getVariables(), requestDataMap);
         }
-        HttpRequest httpRequest = httpUtil.createHttpRequest(requestDataMapper, api.getPath());
-        Map<String, Object> requestDataMap = httpUtil.getHttpRequestDataMap(httpRequest);
-        if (requestDataMap.get("host") == null) {
-            requestDataMap.put("host", requestDataMapper.get("host"));
-            requestDataMap.put("path", StringUtils.hasText((String) requestDataMap.get("queryParam")) ? requestDataMapper.get("host") +  api.getPath() + requestDataMap.get("queryParam") : requestDataMapper.get("host") + api.getPath());
+        HttpRequest httpRequest = httpUtil.createHttpRequest(requestDataMap, path);
+        step.setRequestData(getRequestData(path, httpRequest, requestData));
+
+        Map<String, Object> responseDataMap = sendRequest(httpRequest);
+        step.setResponseData(responseDataMap);
+
+        ScriptExecutionResultVo postScriptResult = executeScript(null, responseDataMap, environmentVariable, (String) requestData.get("postRequestScript"), false);
+        step.setPostScriptData(postScriptResult);
+
+        if (!isChain) {
+            apiRequestLogs.setInterfaceId((Long) requestData.get("interfaceId"));
+            RequestResultVo resultVo = new RequestResultVo();
+            resultVo.setPreExecutionResult(preScriptResult);
+            resultVo.setResponse(responseDataMap);
+            resultVo.setPostExecutionResult(postScriptResult);
+
+            if (environmentVariable != null) {
+                environmentVariableService.updateEnvironmentVariableAsync(environmentVariable, UserContextHolder.getUserId());
+            }
+            return resultVo;
         }
-        requestDataMap.put("preScript", preRequestScript);
-        requestDataMap.put("postScript", postRequestScript);
-        steps.setRequestData(requestDataMap);
+        return null;
+    }
 
-        long start = System.currentTimeMillis();
-        @Cleanup HttpResponse httpResponse = sendRequest(httpRequest);
-        long end = System.currentTimeMillis();
-        Map<String, Object> responseDataMap = httpUtil.getHttpResponseDataMap(httpResponse, end - start);
-        steps.setResponseData(responseDataMap);
-        resultVo.setResponse(responseDataMap);
+    public void runChainRequest(
+            List<InterfaceTestcase> interfaceTestcaseList,
+            Map<Long, String> interfaceIdPath,
+            EnvironmentVariable environmentVariable,
+            Long userId
+    ) {
+        ApiRequestLogs apiRequestLogs = ApiRequestLogsContextHolder.get();
+        if (apiRequestLogs == null) {
+            apiRequestLogs = new ApiRequestLogs();
+        }
+        runChainRequest(interfaceTestcaseList, interfaceIdPath, environmentVariable, apiRequestLogs, userId);
+    }
 
-        ScriptExecutionResultVo postScriptResult = executeScript(null, responseDataMap, environmentVariable, postRequestScript, false);
-        resultVo.setPostExecutionResult(postScriptResult);
-        steps.setPostScriptData(postScriptResult);
-
-        updateEnvironment(environmentVariable);
-        return resultVo;
+    private void runChainRequest(
+            List<InterfaceTestcase> interfaceTestcaseList,
+            Map<Long, String> interfaceIdPath,
+            EnvironmentVariable environmentVariable,
+            ApiRequestLogs apiRequestLogs,
+            Long userId
+    ) {
+        Map<Long, EnvironmentVariable> bindingEnvironment = (environmentVariable == null)
+                ? environmentVariableService.getTestcaseBindingEnvironment(interfaceTestcaseList, userId)
+                : null;
+        for (InterfaceTestcase testcase : interfaceTestcaseList) {
+            try {
+                EnvironmentVariable currentEnv;
+                if (environmentVariable != null) {
+                    // 最高优先级：使用方法传入的全局环境
+                    currentEnv = environmentVariable;
+                } else if (bindingEnvironment != null) {
+                    // 次高优先级：使用用例自身绑定的环境
+                    currentEnv = bindingEnvironment.get(testcase.getId()); // get() 返回 null 也没关系
+                } else {
+                    // 最低优先级：没有环境
+                    currentEnv = null;
+                }
+                runRequest(
+                        interfaceIdPath.get(testcase.getInterfaceId()),
+                        currentEnv,
+                        BeanUtil.beanToMap(testcase),
+                        apiRequestLogs,
+                        true
+                );
+            } catch (Exception e) {
+                int size = apiRequestLogs.getSteps().size();
+                if (size > 0) {
+                    apiRequestLogs.getSteps().get(size - 1).setErrorMsg(e.getMessage());
+                    apiRequestLogs.getSteps().get(size - 1).setExecuteResult(RequestResultConstant.ERROR);
+                }
+            }
+        }
     }
 
     private ScriptExecutionResultVo executeScript(
@@ -114,14 +164,22 @@ public class RunInterfaceTestcaseService {
         );
     }
 
-    private HttpResponse sendRequest(HttpRequest httpRequest) {
-        return httpUtil.sendCustomizeHttpRequest(httpRequest);
+    private Map<String, Object> sendRequest(HttpRequest httpRequest) {
+        long start = System.currentTimeMillis();
+        @Cleanup HttpResponse httpResponse = httpUtil.sendCustomizeHttpRequest(httpRequest);
+        long end = System.currentTimeMillis();
+        return httpUtil.getHttpResponseDataMap(httpResponse, end - start);
     }
 
-    private void updateEnvironment(EnvironmentVariable environmentVariable) {
-        // 更新环境变量
-        if (environmentVariable != null) {
-            environmentVariableMapper.updateById(environmentVariable);
+    private Map<String, Object> getRequestData(String path, HttpRequest httpRequest, Map<String, Object> requestData) {
+        Map<String, Object> requestDataMap = httpUtil.getHttpRequestDataMap(httpRequest);
+        if (requestDataMap.get("host") == null) {
+            requestDataMap.put("host", requestData.get("host"));
+            requestDataMap.put("path", StringUtils.hasText((String) requestDataMap.get("queryParam")) ? requestData.get("host") + path + requestDataMap.get("queryParam") : requestData.get("host") + path);
         }
+        requestDataMap.put("preScript", requestData.get("preRequestScript"));
+        requestDataMap.put("postScript", requestData.get("postRequestScript"));
+
+        return requestDataMap;
     }
 }
